7bb8c07970b0440ec36b2e52c35ef8ee936e5338
sultanaarbiya <115937326+sultanaarbiya@users.noreply.github.com>
2023-11-22 19:34:25 +0530

    number_check.sh

---
+++ number_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# example of break in loop
+# we just need to confirm a given no
+
+no=6
+
+for i in 1 2 3 4 5 6 7 8 9
+do
+	if [[  $no -eq $i ]]
+	then 
+		echo "$no is found"
+		break
+	fi
+	echo "number is $i"
+done
