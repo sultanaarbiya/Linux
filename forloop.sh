022c7c6b60fd546acb41b7775f65a3b637da9f3d
sultanaarbiya <115937326+sultanaarbiya@users.noreply.github.com>
2023-11-22 18:26:07 +0530

    forloop.sh

---
+++ forloop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+	echo "Number is $i"
+done
+
+for name in Arru khan sam saad
+do
+	echo "Name is $name"
+done
+
+for j in {1..50}
+do
+	echo "Number is $j"
+done
